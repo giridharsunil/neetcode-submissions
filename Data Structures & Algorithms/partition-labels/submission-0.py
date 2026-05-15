03bd9eb941f84423a23d2042f4daa346ffe26a1d
Giridhar S <80820437+giridharsunil@users.noreply.github.com>
2026-05-15 19:04:20 -0400

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        map = {}
+        for i in range(len(s)):
+            if s[i] not in map:
+                map[s[i]] = i
+            else:
+                map[s[i]] = i
+        size = 0
+        res = []
+        lastindex = map[s[0]]
+        a = s[0]
+        for i in range(len(s)):
+            size += 1
+            if a != s[i]:
+                lastindex = max(lastindex, map[s[i]])
+                a = s[i]
+            if i == lastindex:
+                res.append(size)
+                size = 0
+        return res
+            
